245b76cfa9f5817415e59499fd5ca97c2b570968
Neamul01 <neamulhaque2002@gmail.com>
2022-09-25 00:14:33 +0600

    nested blog added

---
+++ pages/blog/first.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const first = () => {
+    return (
+        <div>
+            <h2>First Blog</h2>
+        </div>
+    );
+};
+
+export default first;
---
+++ pages/blog/second.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const second = () => {
+    return (
+        <div>
+            <h2>Second Blog</h2>
+        </div>
+    );
+};
+
+export default second;
